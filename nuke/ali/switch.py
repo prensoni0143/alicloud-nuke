30df4fc6d0d577e6b6e953ef8f368c60321de0e3
Huan Song <njuhuansong@gmail.com>
2022-05-22 19:58:21 -0400

    implement list for switch

---
+++ nuke/ali/switch.py
@@ -0,0 +1,34 @@
+import json
+from typing import Dict, List
+from unittest import result
+
+from aliyunsdkvpc.request.v20160428 import DescribeVSwitchesRequest
+from nuke.ali.base import Command
+
+
+class Switch(Command):
+    name = "switch"
+    display_name = "vSwitches"
+
+    def list(self) -> List[Dict[str, str]]:
+        request = DescribeVSwitchesRequest.DescribeVSwitchesRequest()
+        request.set_PageSize = 10
+        request.set_PageNumber = 1
+
+        response: bytes = self.client.do_action_with_exception(request)
+
+        data = json.loads(
+            response.decode("UTF-8")).get("VSwitches", {}).get("VSwitch", [])
+
+        results = []
+        for x in data:
+            results.append({
+                "VSwitchId": x.get("VSwitchId", ""),
+                "VSwitchName": x.get("VSwitchName", ""),
+                "ZoneId": x.get("ZoneId", ""),
+                "CreationTime": x.get("CreationTime", "")
+            })
+        return result
+
+    def delete(self):
+        print("delete vpc")
--- nuke/main.py
+++ nuke/main.py
@@ -1,13 +1,20 @@
-from nuke.ali.vpc import VPC
-
+from nuke.ali.base import Command
+from nuke.ali.switch import Switch
 from nuke.ali.util import regional_clients_registry
+from nuke.ali.vpc import VPC
 
 
 def main():
     for region_id, client in regional_clients_registry.items():
-        vpc = VPC(client=client)
-        print(f"process {region_id}")
-        print(vpc.list())
+        # process_resource(region_id=region_id, resoruce=VPC(client))
+        process_resource(region_id=region_id, resoruce=Switch(client))
+
+
+def process_resource(region_id: str, resoruce: Command):
+    # print(f"list {resoruce.name} in {region_id}")
+    items = resoruce.list()
+    if items:
+        print(items)
 
 
 if __name__ == '__main__':
