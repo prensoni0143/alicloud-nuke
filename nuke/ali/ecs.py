99fa7a8f5cb0f455dfca12cb613c34378ab03a22
Huan Song <njuhuansong@gmail.com>
2022-05-25 23:42:25 -0400

    implement ECS instance

--- nuke/ali/ecs.py
+++ nuke/ali/ecs.py
@@ -1,3 +1,10 @@
+import json
+from typing import Dict, List
+
+from aliyunsdkcore.acs_exception.exceptions import (ClientException,
+                                                    ServerException)
+from aliyunsdkecs.request.v20140526 import (DescribeInstancesRequest,
+                                            DeleteInstanceRequest)
 from nuke.ali.base import Command
 
 
@@ -5,8 +12,46 @@ class ECS(Command):
     name = "ecs"
     display_name = "Elastic Compute Service"
 
-    def list(self):
-        print("list ecs")
+    def list(self) -> List[Dict[str, str]]:
+        results = []
+        page_count = 0
+        total_count = -1
 
-    def delete(self):
-        print("delete ecs")
+        while total_count > self.PAGE_SIZE * page_count or total_count == -1:
+            page_count = page_count + 1
+            request = DescribeInstancesRequest.DescribeInstancesRequest()
+            request.set_PageSize(self.PAGE_SIZE)
+            request.set_PageNumber(page_count)
+
+            response: bytes = self.client.do_action_with_exception(request)
+            r_json = json.loads(response.decode("UTF-8"))
+            print(r_json)
+            total_count = r_json.get("TotalCount")
+            data = r_json.get("Instances", {}).get("Instance", [])
+
+            for x in data:
+                results.append(
+                    {
+                        "InstanceId": x.get("InstanceId", ""),
+                        "InstanceName": x.get("InstanceName", ""),
+                        "RegionId": x.get("RegionId", ""),
+                        "CreationTime": x.get("CreationTime", "")
+                    }
+                )
+        return results
+
+    def delete(self, data: Dict[str, str]):
+        try:
+            id = data.get("InstanceId")
+            name = data.get("InstanceName")
+            request = DeleteInstanceRequest.DeleteInstanceRequest()
+            request.set_InstanceId(id)
+
+            print(f"delete {self.name}: {id} ({name})")
+            response = self.client.do_action_with_exception(request)
+            response_json = json.loads(response)
+            return response_json
+        except ServerException as e:
+            print(f"failed to delete: {e}")
+        except ClientException as e:
+            print(f"failed to delete: {e}")
---
+++ terraform/ecs/main.tf
@@ -0,0 +1,65 @@
+terraform {
+  required_providers {
+    alicloud = {
+      source  = "hashicorp/alicloud"
+      version = "1.168.0"
+    }
+    random = {
+      source  = "hashicorp/random"
+      version = "3.2.0"
+    }
+  }
+}
+
+provider "alicloud" {
+  region = "us-east-1"
+}
+
+resource "random_string" "this" {
+  length  = 8
+  upper   = false
+  special = false
+}
+
+module "vpc" {
+  source  = "alibaba/vpc/alicloud"
+  version = "v1.10.0"
+
+  create   = true
+  vpc_name = "vpc-nuke-us-east-01"
+  vpc_cidr = "172.16.0.0/16"
+
+  availability_zones = ["us-east-1a", "us-east-1b"]
+  vswitch_cidrs      = ["172.16.0.0/18", "172.16.64.0/18"]
+
+  vpc_tags = {
+    Owner       = "nuker"
+    Environment = "dev"
+  }
+}
+
+resource "alicloud_security_group" "group" {
+  name        = "tf_test_foo"
+  description = "foo"
+  vpc_id      = module.vpc.this_vpc_id
+}
+
+resource "alicloud_instance" "instance" {
+  availability_zone          = "us-east-1a"
+  security_groups            = alicloud_security_group.group.*.id
+  instance_type              = "ecs.g6.large"
+  system_disk_category       = "cloud_efficiency"
+  system_disk_name           = "test_foo_system_disk_name"
+  system_disk_description    = "test_foo_system_disk_description"
+  image_id                   = "ubuntu_18_04_64_20G_alibase_20190624.vhd"
+  instance_name              = "test_foo"
+  vswitch_id                 = module.vpc.this_vswitch_ids[0]
+  internet_max_bandwidth_out = 2
+  data_disks {
+    name        = "disk2"
+    size        = 20
+    category    = "cloud_efficiency"
+    description = "disk2"
+    encrypted   = true
+  }
+}
