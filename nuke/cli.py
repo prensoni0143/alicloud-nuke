a1ef72a7cd717ab601f5bb83a06c89b602a119da
Huan Song <njuhuansong@gmail.com>
2022-05-29 12:11:32 -0400

    add confirm deletion

--- nuke/cli.py
+++ nuke/cli.py
@@ -2,6 +2,7 @@ import click
 
 from nuke import service
 from nuke.registry import command_registry
+from nuke.service import resources_list
 
 
 @click.command()
@@ -22,7 +23,24 @@ def nuke(resource_type, all_resources, delete):
     service.list_resources(targets)
 
     if delete:
+        print("\nCommend has '--delete' option")
+        confirm_deletion()
         service.delete_resources(targets)
+
+
+def confirm_deletion():
+    total = 0
+    for k, v in resources_list.items():
+        if len(v) > 0:
+            print(f"Total No. of {k} to delete: {len(v)}")
+            total = total + len(v)
+    val = input(
+        f"Total No. of resources to delete {total}: are you sure? (Y/N)")
+
+    if val.lower() != "y":
+        exit()
+    else:
+        return
 
 
 if __name__ == "__main__":
